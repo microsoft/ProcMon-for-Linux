5e652f3f3858176334c799e573e1fc9bf5c3bf7e
Mario Hewardt <marioh@microsoft.com>
2026-09-08 16:01:54 -0700

    Fix legacy eBPF register definition on ARM64 (#155)

--- src/tracer/ebpf/kern/procmonEBPF_common.h
+++ src/tracer/ebpf/kern/procmonEBPF_common.h
@@ -52,6 +52,37 @@
 
 #define EBPF_RET_UNUSED     0
 
+#ifdef __BPF_TRACING__
+#ifdef EBPF_CO_RE
+typedef struct pt_regs procmon_pt_regs;
+#else
+typedef struct
+{
+    unsigned long r15;
+    unsigned long r14;
+    unsigned long r13;
+    unsigned long r12;
+    unsigned long rbp;
+    unsigned long rbx;
+    unsigned long r11;
+    unsigned long r10;
+    unsigned long r9;
+    unsigned long r8;
+    unsigned long rax;
+    unsigned long rcx;
+    unsigned long rdx;
+    unsigned long rsi;
+    unsigned long rdi;
+    unsigned long orig_rax;
+    unsigned long rip;
+    unsigned long cs;
+    unsigned long eflags;
+    unsigned long rsp;
+    unsigned long ss;
+} procmon_pt_regs;
+#endif
+#endif
+
 struct SyscallEvent
 {
     pid_t pid;
